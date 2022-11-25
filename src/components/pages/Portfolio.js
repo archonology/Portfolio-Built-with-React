ff1d73305088d141b1fe6528460e135b863de3bd
Reed Meher <reed.meher@gmail.com>
2022-11-25 17:26:04 -0600

    projects refactor(props)

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,8 +1,59 @@
 import React, { useState } from 'react'
 import { useSpring, a } from 'react-spring'
+import ProjectCard from './ProjectCard';
 
+const projects = [
+  {
+  _id: 1,
+  customClass: 'card-img-top flip',
+  title: 'Home Party',
+  description: 'A full-stack website built collaboratively. Home Party is a place to invite your family and friends for tracking your favorite homes and all the things you would like to have in it.',
+  repoUrl:'https://github.com/archonology/Home-Party',
+  deployedUrl:'https://pacific-lake-30103.herokuapp.com'
+},
+{
+  _id: 2,
+  customClass: 'card-img-top flip2',
+  title: 'The Wandering Comma',
+  description: 'This application allows for users to sign up, login in, and create and edit posts and comments.',
+  repoUrl:'https://github.com/archonology/The-Wandering-Comma-Tech-Blog',
+  deployedUrl:'https://morning-caverns-74911.herokuapp.com/'
+},
+{
+  _id: 3,
+  customClass: 'card-img-top flip3',
+  title: 'A Night In',
+  description: 'Utilizes public APIs to solve the problem of planning a night home of dining, drinking, and watching a show. This app will help a user find culinary and media inspiration through randomized searches to find ideas.',
+  repoUrl:'https://github.com/archonology/A-Night-In',
+  deployedUrl:'https://archonology.github.io/A-Night-In/'
+},
+{
+  _id: 4,
+  customClass: 'card-img-top flip4',
+  title: 'PWA Text Editor',
+  description: 'Full stack PWA text editor that creates an easy place to keep coding notes or practice functions. Uses Webpack, Workbox, Express, and Idb.',
+  repoUrl:'https://github.com/archonology/PWA-Text-Editor',
+  deployedUrl:'https://stormy-brook-86933.herokuapp.com/'
+},
+{
+  _id: 5,
+  customClass: 'card-img-top flip5',
+  title: 'PWA Text Editor',
+  description: 'A weather tracking website that uses fetch API and localStorage. Needed a site where weather searches were saved so that the user could easily check weather updates while they are traveling.',
+  repoUrl:'https://github.com/archonology/Weather-Dashboard',
+  deployedUrl:'https://archonology.github.io/Weather-Dashboard/'
+},
+{
+  _id: 6,
+  customClass: 'card-img-top flip6',
+  title: 'Simple Scheduler',
+  description: 'Using Javascript and CSS to create a simple work day scheduling application.',
+  repoUrl:'https://github.com/archonology/Work-Day-Scheduler',
+  deployedUrl:'https://archonology.github.io/Work-Day-Scheduler/'
+}
+];
 
-export default function Portfolio() {
+export default function Portfolio(props) {
 
   return (
     <div>
@@ -14,96 +65,20 @@ export default function Portfolio() {
         libero.
       </p>
       <hr></hr>
+        
+  <section className='container d-flex flex-wrap'>
+  <div className='row'>
+{/* map through projects object */}
+{projects.map((project) => (
 
-      <section className='container d-flex flex-wrap'>
-        <div className='row'>
+<div className="project-card col m-3 p-4">
+  <ProjectCard customClass={project.customClass} title={project.title} description={project.description} repoUrl={project.repoUrl} deployedUrl={project.deployedUrl}  />
+</div>
 
-          {/* card 1 */}
-          <div className="project-card col m-3 p-4" >
-            <div className="card-img-top flip">
-            </div>
-            <div className="card-body">
-              <h5 className="card-title">Home Party</h5>
-              <p className="card-text">A full-stack website built collaboratively. Home Party is a place to invite your family and friends for tracking your favorite homes and all the things you would like to have in it.</p>
-              <div className='d-flex justify-content-end'>
-                <a href="https://github.com/archonology/Home-Party" target='_blank' rel='noreferrer' className="link">visit the repo</a>
-                <a href="https://pacific-lake-30103.herokuapp.com" target='_blank' rel='noreferrer' className="link2">visit the live site</a>
-              </div>
-            </div>
-          </div>
+))};
 
-          {/* card 2 */}
-          <div className="project-card col m-3 p-4" >
-            <div className="card-img-top flip2">
-            </div>
-            <div className="card-body">
-              <h5 className="card-title">The Wandering Comma</h5>
-              <p className="card-text"> This application allows for users to sign up, login in, and create and edit posts and comments.</p>
-              <div className='d-flex justify-content-end'>
-                <a href="https://github.com/archonology/The-Wandering-Comma-Tech-Blog" target='_blank' rel='noreferrer' className="link">visit the repo</a>
-                <a href="https://morning-caverns-74911.herokuapp.com/" target='_blank' rel='noreferrer' className="link2">visit the live site</a>
-              </div>
-            </div>
-          </div>
-
-          {/* card 3 */}
-          <div className="project-card col m-3 p-4" >
-            <div className="card-img-top flip3">
-            </div>
-            <div className="card-body">
-              <h5 className="card-title">A Night In</h5>
-              <p className="card-text">Utilizes public APIs to solve the problem of planning a night home of dining, drinking, and watching a show. This app will help a user find culinary and media inspiration through randomized searches to find ideas.</p>
-              <div className='d-flex justify-content-end'>
-                <a href="https://github.com/archonology/A-Night-In" target='_blank' rel='noreferrer' className="link">visit the repo</a>
-                <a href="https://archonology.github.io/A-Night-In/" target='_blank' rel='noreferrer' className="link2">visit the live site</a>
-              </div>
-            </div>
-          </div>
-
-          {/* card 4 */}
-          <div className="project-card col m-3 p-4" >
-            <div className="card-img-top flip4">
-            </div>
-            <div className="card-body">
-              <h5 className="card-title">PWA Text Editor</h5>
-              <p className="card-text">Full stack PWA text editor that creates an easy place to keep coding notes or practice functions. Uses Webpack, Workbox, Express, and Idb.</p>
-              <div className='d-flex justify-content-end'>
-              <a href="https://github.com/archonology/PWA-Text-Editor" target='_blank' rel='noreferrer' className="link">visit the repo</a>
-                <a href="https://stormy-brook-86933.herokuapp.com/" target='_blank' rel='noreferrer' className="link2">visit the live site</a>
-              </div>
-            </div>
-          </div>
-
-          {/* card 5 */}
-          <div className="project-card col m-3 p-4" >
-            <div className="card-img-top flip5">
-            </div>
-            <div className="card-body">
-              <h5 className="card-title">Weather Dashboard</h5>
-              <p className="card-text">A weather tracking website that uses fetch API and localStorage. Needed a site where weather searches were saved so that the user could easily check weather updates while they are traveling.</p>
-              <div className='d-flex justify-content-end'>
-              <a href="https://github.com/archonology/Weather-Dashboard" target='_blank' rel='noreferrer' className="link">visit the repo</a>
-                <a href="https://archonology.github.io/Weather-Dashboard/" target='_blank' rel='noreferrer' className="link2">visit the live site</a>
-              </div>
-            </div>
-          </div>
-
-          {/* card 6 */}
-          <div className="project-card col m-3 p-4" >
-            <div className="card-img-top flip6">
-            </div>
-            <div className="card-body">
-              <h5 className="card-title">Simple Scheduler</h5>
-              <p className="card-text">Using Javascript and CSS to create a simple work day scheduling application.</p>
-              <div className='d-flex justify-content-end'>
-              <a href="https://github.com/archonology/Work-Day-Scheduler" target='_blank' rel='noreferrer' className="link">visit the repo</a>
-                <a href="https://archonology.github.io/Work-Day-Scheduler/" target='_blank' rel='noreferrer' className="link2">visit the live site</a>
-              </div>
-            </div>
-          </div>
-
-          {/* end of site cards */}
-
+</div>
+</section>
 
 
           <h2 className='mt-5'>Back End Demos</h2>
@@ -178,8 +153,8 @@ export default function Portfolio() {
 
         </div>
 
-      </section>
-    </div >
+    //   </section>
+    // </div >
 
 
 
--- src/components/pages/ProjectCard.js
+++ src/components/pages/ProjectCard.js
@@ -4,22 +4,18 @@ import homeParty from '../images/home-party-page.jpg';
 
 export default function ProjectCard(props) {
     return (
-        <div>
-            <div className="project-card col m-3 p-4" >
-                <img
-                    alt={props.image}
-                    className="card-img-top"
-                    src={homeParty}
-                />
+            <div>
+                <div className={props.customClass}>
+                </div>
                 <div className="card-body">
                     <h5 className="card-title">{props.title}</h5>
                     <p className="card-text">{props.description}</p>
                     <div className='d-flex justify-content-end'>
-                        <a href={props.repo} className="link">Visit the Repo</a>
-                        <a href={props.deployed}className="link2">Visit the Live Site</a>
+                        <a href={props.repoUrl} target='_blank' rel='noreferrer' className="link">Visit the Repo</a>
+                        <a href={props.deployedUrl} target='_blank' rel='noreferrer'
+                            className="link2">Visit the Live Site</a>
                     </div>
                 </div>
             </div>
-        </div>
     )
 }
