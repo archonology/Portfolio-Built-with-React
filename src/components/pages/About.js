071d1577bb967519408e7c14513cf671f871131d
Reed Meher <reed.meher@gmail.com>
2022-12-26 17:59:56 -0600

    updated resume and content

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -10,7 +10,7 @@ export default function About() {
         <hr></hr>
       <div className='col col-md-7'>
         <p>
-          Reed Meher (Meherdevs) is a Full Stack Web Developer with a background in writing, marketing, design, and social work.  Refined editorial skills and creative problem solving to develop user-friendly applications. Recognized by employers and clients for kindness, humor, leadership, quality of work, and getting things done with integrity. 
+          Reed Meher (Meherdevs) is a Junior Full Stack Web Developer who implements advanced editorial skills and creative problem solving to develop well-built user-friendly applications. Recognized by employers and clients for kindness, humor, leadership, quality of work, diligence, and getting things done with integrity.
         </p>
         <p>
           Reed lives in Grand Marais, MN, along the edge of the Gitchigumi with his three daughters, his partner, grandparents, and their fur-children, Alma and Reiko. They love to make art, go exploring, cook food together, play games, and have dance parties.
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import resume from '../materials/reedMeher-resume.pdf';
+import resume from '../materials/MeherDevs_2023_Resume.pdf';
 
 export default function Resume() {
   return (
@@ -10,48 +10,37 @@ export default function Resume() {
         <div className='row'>
 
           <div className='resume2 col m-3 mb-1 p-4'>
-            <h4>Key Work Accomplishments:</h4>
-            <br></br>
-            <p> • Top-ranked from client feedback, quality of documentation, critical-thinking skills, and never missing deadlines. (Cook County PHHS)
-            </p>
-            <p>   • Known and recognized by supervisors, attorneys, tribal agencies, and judges for settling cases ahead of trial via advanced skills in mediation, communication, investigation, planning, and collaboration. (Cook County PHHS)
-            </p>
-            <p>  • Recognized for increasing quality of advertising, customer relations, and quality of signage and branding. Store revenue up 22% by 2017. (CCWFC)
-            </p>
-            <p>  • Advanced the quality of curriculum testing, lesson planning, and class materials through research, development, and implementation. (Palisades Academy)
+            <h4>Languages & Specialties:</h4>
+            <p>Javascript, CSS3, HTML5, React, Node, GraphQL, Apollo Server, SQL databases, MERN stack, and RESTful practices
             </p>
 
           </div>
 
           <div className='resume col m-3 mb-1 p-4'>
 
-            <h4>Developer Highlights:</h4>
-            <br></br>
-            <p>   • Clean code written in HTML, CSS, JS with front and backend technologies.
+            <h4>Involvement & Interests:</h4>
+            <p>   javascript30.com, Front End Happy Hour(podcast), new tech, cooking, hiking, art, gaming, music, my  partner, our three daughters, and our fur children, Alma & Reiko.
             </p>
-            <p>   • Experience working on coding teams in an AGILE environment, and flying solo, too, to push out MVP and beyond.
-            </p>
-            <p>   • Always ready to learn, improve, and refactor -- as evidenced in my GitHub commit history for any project.
-            </p>
+
           </div>
 
           <div className='resume row m-2 mt-0 p-4'>
 
             <h4>Education:</h4>
-
-            <p>  <strong> Boot Camp Certificate:</strong> University of Minnesota, Minneapolis, MN </p>
-            <p> A 24-week intensive program focused on gaining technical programming skills in HTML5, CSS3, Javascript, JQuery, Bootstrap, NoSQL, Node.js, MySQL, MongoDB, Express, Handelbars.js, and ReactJS (MERN and RESTFul practices).
+            <p>  <strong> Boot Camp Certificate:</strong> University of Minnesota, Minneapolis, MN | June 2022 - December 2022
+              <br></br>
+              A 24-week intensive program focused on gaining technical programming skills in HTML5, CSS3, Javascript, JQuery, Bootstrap, NoSQL, Node.js, MySQL, MongoDB, Express, Handelbars.js, and ReactJS (MERN and RESTFul practices). <strong>GPA: 4.0+</strong>
             </p>
 
-            <p>  <strong> BFA:</strong> University of Minnesota, Morris, MN </p>
-            <p>  English Literature with a minor in Studio Art.  GPA: 3.74
+            <p>  <strong> BFA:</strong> University of Minnesota, Morris, MN | August 2004 - June 2008
+              <br></br>
+              English Literature with a minor in Studio Art.  <strong>GPA: 3.74</strong>
             </p>
 
           </div>
         </div>
       </div>
-      <h5>Trained in JAVASCRIPT | CSS |  MERN | ORM | HTML | SEQUELIZE | MySQL | + More </h5>
-      
+
       <div className='d-flex justify-content-center'>
         <a href={resume} className='link3' download>Download Resume</a>
       </div>
